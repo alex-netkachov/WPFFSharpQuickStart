7779b115b605d36d01bb6f2b57d11a8a0c3dc668
Alex Netkachov <alex.netkachov@gmail.com>
2017-11-22 01:58:16 +0000

    going reactive

--- MyApp.Libary/Services.fs
+++ MyApp.Libary/Services.fs
@@ -1,6 +1,15 @@
 ﻿namespace MyApp.Library
 
 type WelcomeTextProvider () =
-    member x.GetWelcomeText() =
-        "Hi there. I'm a new awesome app!"
+  let event = new Event<string> ()
 
+  do
+    async {
+        do! Async.Sleep 2000
+        event.Trigger "OK"
+    } |> Async.Start
+
+  member x.GetWelcomeText() =
+    "Hi there. I'm a new awesome app!"
+
+  member x.Text with get() = event.Publish
--- MyApp/MainViewModel.fs
+++ MyApp/MainViewModel.fs
@@ -1,7 +1,14 @@
 ﻿namespace MyApp
 
+open System.Reactive.Concurrency
+open System.Windows
+open ReactiveUI
+
 type MainViewModel () =
   let welcome = WelcomeViewModel ()
+
+  do
+    RxApp.MainThreadScheduler <- DispatcherScheduler(Application.Current.Dispatcher)
 
   member vm.Welcome
     with get() = welcome
